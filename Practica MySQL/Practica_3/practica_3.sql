5cd3902a6720a0c753b11c29d47d6907b3177c7e
velasco182 <ruben.velasco0@misena.edu.co>
2023-05-29 15:40:55 -0500

    Practica 3 29/05/2023

---
+++ Practica MySQL/Practica_3/practica_3.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (codigo, descripcion, precioVenta, precioCompra, existencia)
+VALUES ('DEF456', 'Zapatos deportivos', 75.00, 15.99, 25),
+        ('GHI789', 'Bolígrafo azul', 59.90, 15.00, 100),
+        ('JKL012', 'Pantalón negro', 89.90, 45.00, 10),
+        ('MNO345', 'Gorra roja', 29.90, 12.00, 50),
+        ('PQR678', 'Calcetines blancos', 99.00, 30.00, 75);
